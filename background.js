989dee64379f6277de840a4de722cd922c899716
Paul Repka <repkap11@gmail.com>
2014-12-04 22:38:46 -0500

    Added every image from site to load from the extension for better performance.

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 var lastRequestId;
 chrome.webRequest.onBeforeRequest.addListener(
   function(request) {
-    //console.log("Saw a request"+request.url);
-    lastRequestId = request.requestId;
-    newURL = request.url.replace("http://runetrack.com/images/banner_text.png", chrome.extension.getURL("images/site_banner_text.png"));
-    newURL =  newURL.replace("http://runetrack.com/forums/images/runetrack_banner.jpg", chrome.extension.getURL("images/forum_banner.jpg"));
-      return { 
-          redirectUrl : newURL
-      };
+    console.log(request.url);
+
+    newURL = request.url.replace("http://runetrack.com/forums/images/runetrack_banner.jpg", chrome.extension.getURL("images/forum_banner.jpg"));
+    newURL = newURL.replace("http://runetrack.com/images/", chrome.extension.getURL("images/"));
+    return { 
+      redirectUrl : newURL
+    };
   }, 
   {
       urls : ["*://*.runetrack.com/images/*","*://*.runetrack.com/forums/images/*"]
